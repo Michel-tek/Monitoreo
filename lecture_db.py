65c0ad60a832d71af0f5e074eb60ce1feacfe8e3
Michel Pappalardo <michel@tek-conseil.com>
2019-10-19 13:44:20 -0400

    mise a jour

--- lecture_db.py
+++ lecture_db.py
@@ -1,7 +1,45 @@
+#! /usr/bin/env python3
 # coding: utf-8
+'''
+@author: michel Pappalardo
+prenom : michel
+Created on : 14/10/2019
+fonction du programme : lire les donnees dans une base de donnees sqlite3
 
+'''
+__version__ = '1.0'
+
+'''
+import des librairies
+
+import pdb; pdb.set_trace() permet d'afficher une console pour debuger les variables 
+
+import logging as lg --- librairie permettant de gerer les niveaux d'affichage de message d'erreur 
+
+lg.basicConfig(level=lg.DEBUG) permet d'afficher le niveau d'alarme a afficher
+
+	DEBUG : Informations détaillées dans le but d'en savoir plus sur l'exécution d'une instruction.
+	INFO : Information sur le déroulement d'un programme.
+	WARNING : Quelque chose d'inattendue s'est produit mais le programme continue de fonctionner.
+	ERROR : A cause d'un problème important, le programme n'a pu réaliser une tâche.
+	CRITICAL : Problème très sérieux qui a pu causer l'arrêt du programme.
+	
+	utiliser lg.warning() ou lg.error() a la place d'un print d'information a destination du developpeur
+	ex ; lg.warning('attention le programme est arrete')
+'''
+import logging as lg
 import sqlite3
 import sound
+
+
+lg.basicConfig(level=lg.INFO)
+	
+if __name__ == "__main__":
+    lg.info('script interprété en tant que programe principal')
+else:
+    lg.info('script importé depuis un autre programe')
+
+
 
 precd = 0
 conn = sqlite3.connect('event.db')
