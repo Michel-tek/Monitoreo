cabf9e56a359e69913249d14ad252b9409722a0c
Michel Pappalardo <michel@tek-conseil.com>
2019-10-20 00:39:19 -0400

    ajout de variable de debug

--- lecture_db.py
+++ lecture_db.py
@@ -44,12 +44,15 @@ else:
 precd = 0
 conn = sqlite3.connect('event.db')
 cursor = conn.cursor()
+lg.debug("valeur de la variable cursor',cursor)
 cpt = 0
+
 #cursor.execute("""SELECT * FROM events""")
 #for row in cursor:
 #    print('{0} : {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} | {9} | {10}'.format(row[0], row[1], row[2], row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
 #    if row[9] == 20:
 #        print ("appeler client")
+
 while True:
     cursor.execute("""SELECT * FROM events GROUP BY date_recep HAVING id = MAX(id)""")
     cpt += 1
@@ -57,7 +60,9 @@ while True:
         if row[0] != precd :
             print('{0} : {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} | {9} | {10}'.format(row[0], row[1], row[2], row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
             precd = row[0]
+            lg.debug("row[9]',row[9])
             if row[9] == 4:
+            	lg.info('appeler le client')
                 print ("appeler client")
                 sound.play_effect('drums:Drums_06')
                 
